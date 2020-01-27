98aca7da5bf07e6c0d8da6f624bffc04b8a5bf3d
Finley <zhf79212@gmail.com>
2020-01-27 13:12:02 -0330

    operqtors and fixed issue of contribution not showing

---
+++ operators/src/academy/learningJava/Main.java
@@ -0,0 +1,33 @@
+package academy.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+        int perviousResult = result;
+        System.out.println("perviousResult = " + perviousResult);
+        result = result - 1;
+        System.out.println("3 - 1 =  " + result);
+        System.out.println("perviousResult = " + perviousResult);
+
+        result = result * 10; //2 * 10 = 20
+        System.out.println("2 * 10 =" + result);
+
+        result = result / 5; // 20 / 5 = 4
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3; // the reminder of (4 % 3 = 1)
+        System.out.println("4 % 3 = " + result);
+
+        result++; //result + 1, as same as result --
+        result--;
+
+        result +=  2; //1 + 2 = 3
+        System.out.println("1 + 2 = " + result );
+
+
+
+
+    }
+}
