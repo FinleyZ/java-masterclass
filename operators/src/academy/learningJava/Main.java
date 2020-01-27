52b22dce112f53275f34594a5177819e064d3300
Finley <zhf79212@gmail.com>
2020-01-27 15:46:36 -0330

    "Section 4: Java Tutorial: Expressions, Statements, Code blocks, Methods and more"(4/25) completed

--- operators/src/academy/learningJava/Main.java
+++ operators/src/academy/learningJava/Main.java
@@ -26,6 +26,50 @@ public class Main {
         result +=  2; //1 + 2 = 3
         System.out.println("1 + 2 = " + result );
 
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an Alien");
+            System.out.println("I am scared of Alien");
+        }
+
+        int topScore = 80;
+        if (topScore <100){
+            System.out.println("You got a high score");
+        }
+        int secondTopScore = 81;
+        if((topScore>secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100 ");
+        }
+
+        int newValue = 50;
+        if(newValue ==50){
+            System.out.println("This is an error... ");
+        }
+
+        boolean isCar = false;
+        if (isCar){
+            System.out.println("This is not suppose to happen..");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar){
+            System.out.println("wasCar is true.");
+        }
+
+        //operator Challenge
+
+        double a = 40.00d;
+        double b = 80.00d;
+        double c = (a + b) * 100.00d;
+        double d = c % 40.00;
+        boolean ifTrue = (d==0) ? true : false;
+        System.out.println(ifTrue);
+        if(!ifTrue)
+            System.out.println("Got some reminder");
+
+
+
 
 
 
